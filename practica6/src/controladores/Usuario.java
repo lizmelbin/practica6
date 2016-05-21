51e4b74c27f0819b29e2c370235f3e597f707b1b
Liz Tejada <>
2016-05-21 12:20:48 -0400

    Se agregaron los scripts de la BD. Ademas se agrego la clase Etiqueta y su servicio

---
+++ practica6/src/controladores/Usuario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+/**
+ *
+ * @author tepcurso03
+ */
+public class Usuario {
+    private String username;
+    private  String nombre;
+    private String password;
+    private boolean adminsitrador;
+    private boolean autor;
+           
+    
+}
